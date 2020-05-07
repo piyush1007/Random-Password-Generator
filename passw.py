f19e395bf28ef1b3983abbde073acf8947e27f74
Piyush Sinha <64967275+piyush1007@users.noreply.github.com>
2020-05-07 19:00:30 +0530

    Add files via upload

---
+++ passw.py
@@ -0,0 +1,16 @@
+import random
+wordlst=[]
+special_char=['@','#','&','^']
+data=open("wiki.txt","r")
+for line in data:
+    word=line.split()
+    
+
+    for items in word:
+        if len(items)>5:
+            wordlst.append(items.capitalize())
+wording=random.choice(wordlst)
+schar=random.choice(special_char)
+num=str(random.randint(10,99))
+passw=wording+schar+num
+print(passw)            
